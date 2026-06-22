506622dbeb1e12108fd7d4c11183b8a73d483a7d
MUHAMMEDQULIYEV <quliyevmuhammad405@gmail.com>
2026-06-22 23:14:50 +0400

    22-06-2026 part2

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,File,UploadFile,Form,Depends
 from src.schemas import PostCreate,PostResponse
 from src.db import Post,create_db_and_tables ,get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy import select
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -16,42 +16,34 @@ async def lifespan(app=FastAPI):
 
 app=FastAPI(lifespan=lifespan)
 
-text_post = {
-    1: "Hello this is text message",
-    2: "Heyyo nasilsin adamim",
-    3: "사랑한대 연새",
-    4: "Just grabbing a coffee and getting ready to code.",
-    5: "Bugün hava gerçekten çok güzel, dışarı çıkmak lazım.",
-    6: "한국어 공부는 진짜 재미있지만 문법이 조금 어려워요.",
-    7: "Does anyone know the best way to handle async uploads in FastAPI?",
-    8: "Ne yaparsan yap, aşk ile yap!",
-    9: "오늘도 좋은 하루 보내세요! 파이팅!",
-    10: "Finally fixed that stubborn memory leak. Time to celebrate!"
-}
-
-@app.get("/posts")
-def get_all_post(limit:int=None):
-    if limit:
-     return list(text_post.values())[:limit]
-    return text_post
+@app.post("/upload")
+async def upload_file(
+    file:UploadFile=File(...),
+    caption:str=Form(""),
+    session:AsyncSession=Depends(get_async_session)
+):
+ post=Post(
+    caption=caption,
+    url="dummyurl",
+    file_type="dummy name"
+ )
+ session.add(post)
+ await session.commit()
+ await session.refresh(post)
 
 
-
-@app.get("/post/{id}")
-def get_specific_post(id:int):
-    if id not in text_post:
-        raise HTTPException(status_code=404,detail="Post not found")
-    else:
-     return text_post.get(id)
-     
-
-@app.post("/posts")
-def create_post(post: PostCreate)->PostResponse:
-    # 1. Calculate the next available ID number
-    new_id = max(text_post.keys()) + 1
-    
-    # 2. Save the incoming content to your dictionary
-    text_post[new_id] = post.content
-    
-    # 3. Return the newly created item so the user knows it worked
-    return {"id": new_id, "post": text_post[new_id]}
+@app.get("/feed")
+async def get_feed(session:AsyncSession=Depends(get_async_session)):
+  result= await session.execute(select(Post).order_by(Post.created_at.desc()))
+  post= [row[0] for row in result.all()]
+  post_data=[]
+  for post in post:
+    post_data.append({
+        "id":str(post.id),
+        "caption":post.caption,
+        "url":post.url,
+        file_type:post.file_type,
+        file_name:post.file_name,
+        created_at:post.create_at.isformat()
+    })
+    return {"posts":post_data}
